9f1aec247717806c3180a9ae98147df9365e5577
Vansh <vanshpatwari2001@gmail.com>
2026-04-22 00:56:26 +0530

    Flask scalable app with AWS EC2, RDS, Load Balancer

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import pymysql
+
+app = Flask(__name__)
+
+# DB connection
+connection = pymysql.connect(
+    host="YOUR_RDS_ENDPOINT",
+    user="admin",
+    password="YOUR_PASSWORD",
+    database="clinic"
+)
+
+@app.route("/")
+def home():
+    return "Flask + RDS Running 🚀"
+
+@app.route("/book", methods=["POST"])
+def book():
+    data = request.json
+
+    cursor = connection.cursor()
+
+    query = "INSERT INTO bookings (name, date) VALUES (%s, %s)"
+    cursor.execute(query, (data["name"], data["date"]))
+
+    connection.commit()
+
+    return jsonify({"message": "Booking saved in DB"})
+
+@app.route("/bookings", methods=["GET"])
+def get_bookings():
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM bookings")
+    results = cursor.fetchall()
+
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
